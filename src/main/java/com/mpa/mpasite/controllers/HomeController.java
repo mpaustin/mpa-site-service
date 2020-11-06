14ccc5e37e519d78637f6cbbb89a600a353c5bb3
Matt Austin <mattaustin@matts-mbp-3.lan>
2020-11-06 17:23:48 -0500

    changing request mapping to / from /home

--- src/main/java/com/mpa/mpasite/controllers/HomeController.java
+++ src/main/java/com/mpa/mpasite/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @RequestMapping(value = "/home")
+    @RequestMapping(value = "/")
     public String index() {
         return "index";
     }
