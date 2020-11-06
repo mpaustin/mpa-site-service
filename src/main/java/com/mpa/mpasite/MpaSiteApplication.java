909663f1a5a979ea130c03dd496b70ab1086e70e
Matt Austin <mattaustin@matts-mbp-3.lan>
2020-11-06 16:18:22 -0500

    initial commit

---
+++ src/main/java/com/mpa/mpasite/MpaSiteApplication.java
@@ -0,0 +1,13 @@
+package com.mpa.mpasite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpaSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpaSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpa/mpasite/MpaSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpa.mpasite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpaSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
